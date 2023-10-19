d2e3c19fb49393f087810c9db931b995a6dad1e0
Yan Caue <coldecmastercrelan@gmail.com>
2023-10-19 12:18:50 -0300

    novos arquivos

--- src/components/ProjectsContent/index.tsx
+++ src/components/ProjectsContent/index.tsx
@@ -1,12 +1,7 @@
 import { RepositorysList } from '../RepositorysList';
 import styles from './styles.module.scss'
-interface RepositoryListProps {
-    name: string;
-    full_name: string,
-    html_url: string,
-}
 
-export function ProjectsContent(props: RepositoryListProps) {
+export function ProjectsContent() {
   return (
     <section className={styles.container}>
       <h5>My repositorys on Github:</h5>
